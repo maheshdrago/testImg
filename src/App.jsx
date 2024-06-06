29138804fb5fc01af1d8afbdf1336ca58a45ce80
maheshdrago <maheshdrago@gmail.com>
2024-06-05 21:21:29 -0400

    added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <img src='https://imgpile.com/p/Tx5By7H#BFncqUG'/>
+    </>
+  )
+}
+
+export default App
